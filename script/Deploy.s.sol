9033d4d649b93ee223f7b39eea16f1ff0eea2055
echo <hujw77@gmail.com>
2024-02-05 17:35:10 +0800

    Init

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.13;
 import {Script, console2} from "forge-std/Script.sol";
 import {Common} from "create3-deploy/script/Common.s.sol";
 import {ScriptTools} from "create3-deploy/script/ScriptTools.sol";
-import {Counter} from "../src/Counter.sol";
+import {Counter} from "../src/KTONStakingRewards.sol";
 
 contract Deploy is Common {
     address immutable ADDR = 0x00C4B0e8c35a42EfA8ECb67b2155a9A573A632E7;
@@ -19,7 +19,7 @@ contract Deploy is Common {
     }
 
     function run() public broadcast {
-        bytes memory byteCode = type(Counter).creationCode;
+        bytes memory byteCode = type(KTONStakingRewards).creationCode;
         address addr = _deploy3(SALT, byteCode);
         require(addr == ADDR, "!addr");
     }
