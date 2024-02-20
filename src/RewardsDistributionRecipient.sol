fc0af85ce691f25960c3b13a5c1bb7bdea456472
echo <hujw77@gmail.com>
2024-02-20 16:48:23 +0800

    fix

--- src/RewardsDistributionRecipient.sol
+++ src/RewardsDistributionRecipient.sol
@@ -4,7 +4,7 @@ contract RewardsDistributionRecipient {
     // "sc/ktnstk" in bytes.
     address public constant rewardsDistribution = 0x73632F6b746e73746B0000000000000000000000;
 
-    function notifyRewardAmount() external;
+    function notifyRewardAmount(uint256 reward) external;
 
     modifier onlyRewardsDistribution() {
         require(msg.sender == rewardsDistribution, "Caller is not RewardsDistribution contract");
