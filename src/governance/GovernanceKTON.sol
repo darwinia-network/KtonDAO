39db499b5645f4985c7bbdcc6346c2cd3ec8e25e
echo <hujw77@gmail.com>
2024-06-19 17:00:31 +0800

    gKTON

--- src/governance/GovernanceKTON.sol
+++ src/governance/GovernanceKTON.sol
@@ -1,35 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20PermitUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20VotesUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20WrapperUpgradeable.sol";
-import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Wrapper.sol";
 
-contract GovernanceRing is
-    Initializable,
-    ERC20Upgradeable,
-    ERC20PermitUpgradeable,
-    ERC20VotesUpgradeable,
-	ERC20WrapperUpgradeable
-{
-
+contract GovernanceRing is ERC20, ERC20Permit, ERC20Votes, ERC20Wrapper {
     IERC20 public constant KTON = IKTON(0x0000000000000000000000000000000000000402);
 
-    function initialize() public initializer {
-        __ERC20_init("Governance KTON", "gKTON");
-        __ERC20Permit_init(symbol);
-        __ERC20Votes_init();
-		__ERC20Wrapper_init(KTON);
-    }
+    constructor() ERC20("Governance KTON", "gKTON") ERC20Permit("Governance KTON") ERC20Wrapper(KTON) {}
 
-    /// @custom:oz-upgrades-unsafe-allow constructor
-    constructor() {
-        _disableInitializers();
-    }
-
-    function decimals() public view override(ERC20Upgradeable, ERC20WrapperUpgradeable) returns (uint8) {
+    function decimals() public view override(ERC20, ERC20Wrapper) returns (uint8) {
         return super.decimals();
     }
 
@@ -41,10 +23,7 @@ contract GovernanceRing is
         return "mode=timestamp";
     }
 
-    function _update(address from, address to, uint256 value)
-        internal
-        override(ERC20Upgradeable, ERC20VotesUpgradeable)
-    {
+    function _update(address from, address to, uint256 value) internal override(ERC20, ERC20Votes) {
         super._update(from, to, value);
     }
 
